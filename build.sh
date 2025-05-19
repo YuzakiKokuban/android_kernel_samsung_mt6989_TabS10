ed0477cfe57d5c81f54ede4a95b973bcdbff1524
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-19 22:43:35 +0800

    加入三星开源内核源码AXI9

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+# download toolchain from https://opensource.samsung.com/uploadSearch?searchValue=toolchain 
+TOOLCHAIN=$(realpath "/home/kokuban/PlentyofToolchain/toolchainS24/prebuilts")
+
+export PATH=$TOOLCHAIN/build-tools/linux-x86/bin:$PATH
+export PATH=$TOOLCHAIN/build-tools/path/linux-x86:$PATH
+export PATH=$TOOLCHAIN/clang/host/linux-x86/clang-r487747c/bin:$PATH
+export PATH=$TOOLCHAIN/clang-tools/linux-x86/bin:$PATH
+export PATH=$TOOLCHAIN/kernel-build-tools/linux-x86/bin:$PATH
+
+echo $PATH
+
+TARGET_DEFCONFIG=${1:-mt6989_defconfig}
+
+cd "$(dirname "$0")"
+
+
+LOCALVERSION=-android14-Kokuban-Exusiai-AXI9-DEV
+
+if [ "$LTO" == "thin" ]; then
+  LOCALVERSION+="-thin"
+fi
+
+ARGS="
+CC=clang
+ARCH=arm64
+LLVM=1 LLVM_IAS=1
+LOCALVERSION=$LOCALVERSION
+"
+
+# build kernel
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS} $TARGET_DEFCONFIG
+
+./scripts/config --file out/.config \
+  -d UH \
+  -d RKP \
+  -d KDP \
+  -d SECURITY_DEFEX \
+  -d INTEGRITY \
+  -d FIVE \
+  -d TRIM_UNUSED_KSYMS
+
+if [ "$LTO" = "thin" ]; then
+  ./scripts/config --file out/.config -e LTO_CLANG_THIN -d LTO_CLANG_FULL
+fi
+
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS}
+
+cd out
+if [ ! -d AnyKernel3 ]; then
+  git clone --depth=1 https://github.com/YuzakiKokuban/AnyKernel3.git -b mt6989
+fi
+cp arch/arm64/boot/Image AnyKernel3/zImage
+name=TabS10_kernel_`cat include/config/kernel.release`_`date '+%Y_%m_%d'`
+cd AnyKernel3
+zip -r ${name}.zip * -x *.zip
+cd ..
+cp arch/arm64/boot/Image AnyKernel3/tools/kernel
+cd AnyKernel3/tools
+chmod +x libmagiskboot.so
+lz4 boot.img.lz4
+./libmagiskboot.so repack boot.img ${name}.img 
+echo "boot.img output to $(realpath $name).img"
+cd ..
+cd ..
+echo "AnyKernel3 package output to $(realpath $name).zip"
