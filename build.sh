ae88a66b654d8a191887e3229aa4bcfe77aea242
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-25 12:39:43 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@
 set -e
 
 # download toolchain from https://opensource.samsung.com/uploadSearch?searchValue=toolchain 
-TOOLCHAIN=$(realpath "/home/kokuban/PlentyofToolchain/toolchainS24/prebuilts")
+TOOLCHAIN=$(realpath "/home/kokuban/PlentyofToolchain/toolchainTS10/prebuilts")
 
 export PATH=$TOOLCHAIN/build-tools/linux-x86/bin:$PATH
 export PATH=$TOOLCHAIN/build-tools/path/linux-x86:$PATH
 export PATH=$TOOLCHAIN/clang/host/linux-x86/clang-r487747c/bin:$PATH
-export PATH=$TOOLCHAIN/clang-tools/linux-x86/bin:$PATH
-export PATH=$TOOLCHAIN/kernel-build-tools/linux-x86/bin:$PATH
 
 echo $PATH
 
